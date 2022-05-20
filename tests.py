22ffef7911f035dcb2d411b145af27f20712e7f2
Test <a.gorchakovv@gmail.com>
2022-05-20 13:15:19 +0600

    gitignore

--- tests.py
+++ tests.py
@@ -1,13 +1,3 @@
-#    More info about pytest-selenium:
-#    https://pytest-selenium.readthedocs.io/en/latest/user_guide.html
-#
-#    How to run:
-#  1) Install pytest-selenium, selenium
-#  2) Download geko driver for Chrome here:
-#     https://sites.google.com/a/chromium.org/chromedriver/downloads
-#  2) Run tests:
-#     pytest tests.py
-
 from AHPages import SearchHelper
 
 #Открываем сайт, кликаем по кнопке установки и сравниваем полученный url с нужным
