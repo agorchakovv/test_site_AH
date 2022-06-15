20aac0b2c8b7efe5a48081d18703bef9f951bfc2
Test <a.gorchakovv@gmail.com>
2022-06-15 16:06:17 +0600

    status 200

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from AHPages import SearchHelper
+import requests
 
 #Открываем сайт, кликаем по кнопке установки и сравниваем полученный url с нужным
 def test_install(browser):
     main_page = SearchHelper(browser)
     main_page.go_to_site()
-    #window_before = browser.window_handles[0]   #Запоминаем предыдущую страницу
+    window_before = browser.window_handles[0]   #Запоминаем предыдущую страницу
     main_page.Click_button_install()
     window_after = browser.window_handles[1]     #Запоминаем новую страницу
     browser.switch_to.window(window_after)       #Перемещаемся на новую страницу
     assert browser.current_url == "https://chrome.google.com/webstore/detail/alihelper-shopping-assist/ojclfkinnapkabameogjppmeedlicean"
+    response = requests.head(browser.current_url)
+    assert response.status_code == 200
+    
